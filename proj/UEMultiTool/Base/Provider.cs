416aad4a8f68961cecfdb1d4b0da17e229bfc39a
Storm <87811650+Storm99999@users.noreply.github.com>
2021-11-10 14:07:57 +0100

    Add files via upload

---
+++ proj/UEMultiTool/Base/Provider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UEMultiTool.src.Base
+{
+    internal class Provider
+    {
+        public enum PROVIDER_TYPE
+        {
+            GET,
+            AES_PROVIDER
+        }
+    }
+}
---
+++ proj/UEMultiTool/Base/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UEMultiTool.src.Base
+{
+    internal class Service
+    {
+        public enum SERVICE_TYPE
+        {
+            READ,
+            WRITE,
+            GRAB
+        }
+    }
+}
